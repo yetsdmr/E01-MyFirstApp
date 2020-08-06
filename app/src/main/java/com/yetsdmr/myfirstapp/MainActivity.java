bda767700361478886c61a96cd5780a5354651b2
EMRE TAŞDEMİR <yetsdmr@gmail.com>
2020-08-06 13:24:24 +0300

    finished

--- app/src/main/java/com/yetsdmr/myfirstapp/MainActivity.java
+++ app/src/main/java/com/yetsdmr/myfirstapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.yetsdmr.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage(View view){
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.developer2);
     }
 }
